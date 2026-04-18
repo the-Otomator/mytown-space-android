2d5cbb764a1c61b3ee71e210a2e13490b9ff6c44
the-Otomator <156667441+the-Otomator@users.noreply.github.com>
2026-04-18 15:20:54 +0300

    Update versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "space.mytown.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
